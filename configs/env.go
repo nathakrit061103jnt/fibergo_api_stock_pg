ec2fc3ba08046f1423b637fbb651595c94e3ca0d
Nathakrit Tissatian <std60010912540@acc.msu.ac.th>
2022-02-16 01:14:21 +0700

    add

--- configs/env.go
+++ configs/env.go
@@ -13,7 +13,7 @@ func Config(key string) string {
 	// load .env file
 	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Print("Error loading .env file")
+		fmt.Print("Error loading .env file ")
 	}
 	// Return the value of the variable
 	return os.Getenv(key)
